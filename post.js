b52aa79c3618b4b09bdb10b466c7255cbb789e78
Jonathan@home <elastijon@gmail.com>
2015-10-28 11:58:05 +0000

    Slimmed down post

--- post.js
+++ post.js
@@ -2,44 +2,12 @@
 
 var req = require('request-promise');
 var secrets = require('./secrets.private.json');
-var argv = require('yargs').usage('Usage: $0 <command> [options]').argv;
-
-var TEXT, IMAGE, NAME, CHANNEL, USER;
+var argv = require('yargs').argv;
 
 if (argv.h || argv.help){
-  console.log('-t is text, -c is channel, -i is image, -n is username, -u is as_user. Or --call for a simple method that is not postmessage without params.')
-  process.exit(code=0);
-}
-
-if (argv.t){
-  TEXT = argv.t;
-} else {
-  console.log("Need text! Give me some t");
-}
-
-if (argv.c){
-  CHANNEL = argv.c;
-} else {
-  CHANNEL = ""
-  //CHANNEL = "C07191CTG" // General chat
-}
-
-if (argv.image || argv.i){
-  IMAGE = argv.image || argv.i;
-} else {
-  IMAGE = 'http://www.clipartlord.com/wp-content/uploads/2012/12/robot-head.png'
-}
-
-if (argv.n || argv.name){
-  NAME = argv.n || argv.name;
-} else {
-  NAME = 'A Robot';
-}
-
-if (argv.u || argv.user){
-  USER = true;
-} else {
-  USER = false;
+  console.log('\nA simple CLI for posting to Slack.\n\t -t = text of post\n\t -c = channel or user to post to\n\t -i = URL of image for poster\n\t -n = username of poster\n\t -u = boolean flag to post as yourself\n\t --call = call simple method without params that is not postmessage\n')
+  console.log('Examples:\n\tnode post.js -n PressUpBot -t "Bring Sally up, bring sally down... Time to do 10 press ups!" -i http://angrytrainerfitness.com/wp-content/uploads/2012/03/pushupmilitary.jpg -c @paulgwyther\n\tnode post.js --call channels.list\n')
+  process.exit(0);
 }
 
 function api(method, args){
@@ -47,23 +15,10 @@ function api(method, args){
   params.uri = 'https://slack.com/api/' + method + '?token=' + secrets.token;
   params.method = 'POST'
   params.form = args
-  console.log(params)
   return req(params)
 }
 
-var serialiseObject = function(obj) {
-    var pairs = [];
-    for (var prop in obj) {
-        if (!obj.hasOwnProperty(prop)) {
-            continue;
-        }
-        pairs.push(prop + '=' + obj[prop]);
-    }
-    return pairs.join('&');
-}
-
 if (argv.call){
-  console.log("Not posting. Calling: " + argv.call)
   if (argv.call){
     api(argv.call, {
       'token' : secrets.token
@@ -72,10 +27,10 @@ if (argv.call){
 } else {
   api('chat.postMessage', {
     'token' : secrets.token,
-    'channel': CHANNEL,
-    'text': TEXT,
-    'username': NAME,
-    'as_user': USER,
-    'icon_url': IMAGE
+    'channel': argv.c || "",
+    'text': argv.t || "",
+    'username': argv.n || "Slackbot",
+    'as_user': argv.u || false,
+    'icon_url': argv.i || ""
   }).then(console.dir).catch(console.dir)
 }
